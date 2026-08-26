2ca9fa8fe8bb9cf1d8939f5fc3e48510749b0f59
Okeke Chukwuemeka Daniel <danielokeke2004@gmail.com>
2026-08-26 16:22:55 +0100

    Preliminary

---
+++ script.js
@@ -0,0 +1,40 @@
+var navbarToggler = document.querySelector('.navbar-toggler');
+var navbarCollapse = document.querySelector('#main_nav');
+
+if (navbarToggler && navbarCollapse) {
+	navbarToggler.addEventListener('click', function () {
+		var isOpen = navbarCollapse.classList.toggle('show');
+		navbarToggler.setAttribute('aria-expanded', String(isOpen));
+	});
+}
+
+document.querySelectorAll('.navbar .dropdown-toggle').forEach(function (toggle) {
+	toggle.addEventListener('click', function (event) {
+		event.preventDefault();
+		var dropdown = toggle.closest('.dropdown');
+		var isOpen = dropdown.classList.contains('show');
+
+		document.querySelectorAll('.navbar .dropdown.show').forEach(function (openDropdown) {
+			openDropdown.classList.remove('show');
+			openDropdown.querySelector('.dropdown-menu').classList.remove('show');
+			openDropdown.querySelector('.dropdown-toggle').setAttribute('aria-expanded', 'false');
+		});
+
+		if (!isOpen) {
+			dropdown.classList.add('show');
+			dropdown.querySelector('.dropdown-menu').classList.add('show');
+			toggle.setAttribute('aria-expanded', 'true');
+		}
+	});
+});
+
+document.addEventListener('click', function (event) {
+	if (!event.target.closest('.navbar .dropdown')) {
+		document.querySelectorAll('.navbar .dropdown.show').forEach(function (openDropdown) {
+			openDropdown.classList.remove('show');
+			openDropdown.querySelector('.dropdown-menu').classList.remove('show');
+			openDropdown.querySelector('.dropdown-toggle').setAttribute('aria-expanded', 'false');
+		});
+	}
+});
+
